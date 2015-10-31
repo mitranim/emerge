48983cdc3e2a2be876750f6630142dd5cfc856ae
Mitranim <me@mitranim.com>
2015-10-31 15:17:22 +0300

    init

---
+++ readme.md
@@ -0,0 +1,171 @@
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](http://standardjs.com)
+
+## Description
+
+Utilities for creating and working with immutable data. Plain objects only:
+no OOP, no classes you must inherit from, no custom methods. Use your own data.
+
+Features efficient deep merging and referential equality. Helpful for systems
+built with immutability in mind. Food for thought:
+[[1]](https://github.com/matthiasn/talk-transcripts/blob/master/Hickey_Rich/AreWeThereYet.md).
+
+Example:
+
+```javascript
+import {immute, mergeAtRoot} from 'emerge'
+
+const prev = immute({
+  one: [1],
+  two: {three: 3}
+})
+
+const next = {
+  two: {three: 'three'}
+}
+
+// Result of deep merge, immutable.
+const tree = mergeAtRoot(prev, next)
+
+// Unchanged data is referentially equal.
+console.assert(tree.one === prev.one)
+
+// Changed parts.
+console.assert(tree.two.three === 'three')
+```
+
+## API
+
+### `readAtPath(tree, path)`
+
+Takes a data tree and safely reads a value at the given path. If the path is
+unreachable, returns `undefined`.
+
+```javascript
+import {readAtPath} from 'emerge'
+
+const tree = {
+  one: {two: {three: 3}}
+}
+
+console.assert(readAtPath(tree, ['one', 'two', 'three']) === 3)
+```
+
+### `replaceAtRoot(prev, next)`
+
+Creates a new immutable version of the given tree, deep-replaced by the given
+structure starting at the root. Preserves as many original references as
+possible. The original is unaffected.
+
+Returns the original reference is the result would be deep-equal.
+
+```javascript
+import {replaceAtRoot} from 'emerge'
+
+const prev = {
+  one: {two: 2},
+  three: 3
+}
+
+const next = {
+  one: {two: 2},
+  three: 'three'
+}
+
+const tree = replaceAtRoot(prev, next)
+
+console.assert(tree.three === 'three')
+console.assert(tree.one === prev.one)
+```
+
+### `mergeAtRoot(prev, patch)`
+
+Creates a new immutable version of the given tree, deep-patched by the given
+structure starting at the root. Preserves as many original references as
+possible. The original is unaffected.
+
+Returns the original reference is the result would be deep-equal.
+
+```javascript
+import {mergeAtRoot} from 'emerge'
+
+const prev = {
+  one: {
+    two: 2,
+    three: [3]
+  }
+}
+
+const patch = {
+  one: {two: 'two'}
+}
+
+const tree = mergeAtRoot(prev, patch)
+
+console.assert(tree.one.two === 'two')
+console.assert(tree.one.three === prev.one.three)
+```
+
+### `replaceAtPath(prev, value, path)`
+
+Creates a new immutable version of the given tree, patched with the given value
+at the given path. The path must be an array of strings or symbols. Preserves as
+many original references as possible. The original is unaffected.
+
+Returns the original reference is the result would be deep-equal.
+
+```javascript
+import {replaceAtPath} from 'emerge'
+
+const prev = {
+  one: {
+    two: 2,
+    three: 3
+  },
+  four: [4]
+}
+
+const tree = replaceAtPath(prev, 'two', ['one', 'two'])
+
+console.assert(tree.one.two === 'two')
+console.assert(tree.four === prev.four)
+```
+
+### `deepEqual(one, other)`
+
+True if the values are deeply equal, ignoring prototypes and non-enumerable
+properties.
+
+```javascript
+import {deepEqual} from 'emerge'
+
+const prev = {one: NaN, two: [2]}
+const next = {one: NaN, two: [2]}
+
+console.assert(deepEqual(prev, next))
+```
+
+### `immute()`
+
+Deep-freezes the given value, making it immutable. Mutation attemps will throw
+errors in strict mode and silently fail in loose mode.
+
+```javascript
+import {immute} from 'emerge'
+
+const tree = immute({
+  one: 1
+})
+
+let error
+try {
+  tree.one = 2
+} catch (err) {
+  error = err
+} finally {
+  console.assert(!!error)
+}
+```
+
+## Compatibility
+
+Any ES5 environment (IE9+).
---
+++ test/test.js
@@ -0,0 +1,235 @@
+'use strict'
+
+// Main.
+const readAtPath = require('../lib/emerge').readAtPath
+const replaceAtRoot = require('../lib/emerge').replaceAtRoot
+const mergeAtRoot = require('../lib/emerge').mergeAtRoot
+const replaceAtPath = require('../lib/emerge').replaceAtPath
+// Secondary.
+const deepEqual = require('../lib/emerge').deepEqual
+const immute = require('../lib/emerge').immute
+
+let prev, next, tree, error
+
+/**
+ * readAtPath
+ */
+
+prev = next = tree = error = undefined
+
+tree = immute({
+  one: 1,
+  two: {three: {four: [4, 5]}}
+})
+
+if (readAtPath(tree, []) !== tree) throw Error()
+if (readAtPath(tree, ['two']) !== tree.two) throw Error()
+if (readAtPath(tree, ['one']) !== 1) throw Error()
+if (readAtPath(tree, ['two', 'three', 'four', '0']) !== 4) throw Error()
+if (readAtPath(tree, [Symbol()]) !== undefined) throw Error()
+
+/**
+ * replaceAtRoot
+ */
+
+prev = next = tree = error = undefined
+
+prev = immute({
+  one: {two: NaN},
+  three: {four: 4},
+  five: [5]
+})
+
+next = immute({
+  one: {two: NaN},
+  three: {four: [4]},
+  six: 6
+})
+
+tree = replaceAtRoot(prev, next)
+
+// Must be a non-referential copy of the next tree.
+if (tree === prev || tree === next) throw Error()
+if (!deepEqual(tree, next)) throw Error()
+// Referential equality: keep old references if values are deep equal.
+if (tree.one !== prev.one) throw Error()
+// New values must be deep cloned.
+if (tree.three === next.three) throw Error()
+if (tree.three.four === next.three.four) throw Error()
+if (!deepEqual(tree.three, next.three)) throw Error()
+// New values must be immutable.
+try {
+  tree.three.four.push(5)
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+}
+// The new tree must be immutable.
+error = undefined
+try {
+  tree.new = true
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+}
+
+// Must remain the same reference if the result is deep equal.
+next = immute({
+  one: {two: NaN},
+  three: {four: 4},
+  five: [5]
+})
+tree = replaceAtRoot(prev, next)
+if (tree !== prev) throw Error()
+
+/**
+ * mergeAtRoot
+ */
+
+prev = next = tree = error = undefined
+
+prev = immute({
+  one: {two: NaN},
+  three: {four: [4]},
+  six: undefined
+})
+
+next = immute({
+  three: {five: 5},
+  six: [6],
+  seven: 7
+})
+
+tree = mergeAtRoot(prev, next)
+
+if (tree === prev || tree === next) throw Error()
+if (deepEqual(tree, next)) throw Error()
+// Keep unaffected values, with unchanged references.
+if (tree.one !== prev.one) throw Error()
+if (tree.three.four !== prev.three.four) throw Error()
+// Must deep-patch new paths and values in, deep-cloning them.
+if (tree.three.five !== next.three.five) throw Error()
+if (tree.six === next.six) throw Error()
+if (!deepEqual(tree.six, next.six)) throw Error()
+if (tree.seven !== next.seven) throw Error()
+// New values must be immutable.
+try {
+  tree.six.push(7)
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+}
+// The new tree must be immutable.
+error = undefined
+try {
+  tree.new = true
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+}
+
+// Must remain the same reference if the result is deep equal.
+next = immute({
+  one: {two: NaN}
+})
+tree = mergeAtRoot(prev, next)
+if (tree !== prev) throw Error()
+
+/**
+ * replaceAtPath
+ */
+
+prev = next = tree = error = undefined
+
+prev = immute({
+  one: {two: NaN},
+  three: {four: {six: [6]}, five: 5}
+})
+
+next = immute({
+  six: 6
+})
+
+tree = replaceAtPath(prev, next, ['three', 'four'])
+
+if (tree === prev || tree === next) throw Error()
+// Must deep-patch the new value in, deep-cloning it.
+if (tree.three.four === next) throw Error()
+if (!deepEqual(tree.three.four, next)) throw Error()
+// Unaffected paths must remain untouched, with same references.
+if (tree.three.five !== prev.three.five) throw Error()
+// The patched value must be immutable.
+try {
+  tree.three.four.six = 7
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+}
+// The new tree must be immutable.
+error = undefined
+try {
+  tree.new = true
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+}
+
+// Must remain completely unchanged if the result is deep equal.
+next = immute({
+  six: [6]
+})
+tree = replaceAtPath(prev, next, ['three', 'four'])
+if (tree !== prev) throw Error()
+
+/**
+ * deepEqual
+ */
+
+if (!deepEqual({
+  one: {two: {three: NaN}},
+  four: [4, 4],
+  five: 'five'
+}, {
+  one: {two: {three: NaN}},
+  four: [4, 4],
+  five: 'five'
+})) throw Error()
+
+/**
+ * immute
+ */
+
+prev = next = tree = error = undefined
+
+tree = immute({
+  one: {two: NaN},
+  five: {six: [6]}
+})
+
+if (tree.five.six[0] !== 6) throw Error()
+
+try {
+  tree.one = 1
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+  error = undefined
+}
+
+try {
+  tree.five.six.push(7)
+} catch (err) {
+  error = err
+} finally {
+  if (!error) throw Error()
+  error = undefined
+}
+
+console.info(`[${new Date().getUTCHours()}:${new Date().getUTCMinutes()}:${new Date().getUTCSeconds()}] Finished test, no errors.`)
